113e97cb37ba28d884ee6280c826aea4017c53a9
hua <huagua>
2019-11-18 13:05:34 +0800

    添加了网络配置，以及修改天气界面的actionbar

--- app/src/main/java/com/example/carry_weather/weatherActivity.java
+++ app/src/main/java/com/example/carry_weather/weatherActivity.java
@@ -2,10 +2,13 @@ package com.example.carry_weather;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.preference.PreferenceManager;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -59,6 +62,12 @@ public class weatherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
+
+        if(Build.VERSION.SDK_INT>=21){  //版本号判断（以下功能只在21及以上版本实现）
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_STABLE);   //活动的布局会显示在状态栏上面
+            getWindow().setStatusBarColor(Color.TRANSPARENT);   //将状态栏设置为透明色
+        }
 
         setContentView(R.layout.activity_weather);
 
