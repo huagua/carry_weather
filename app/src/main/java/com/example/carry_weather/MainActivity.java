827440aad1c1006c8be312c1ef18254fc239e66c
hua <huagua>
2019-11-18 13:24:57 +0800

    修改了选择城市界面的action bar

--- app/src/main/java/com/example/carry_weather/MainActivity.java
+++ app/src/main/java/com/example/carry_weather/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.carry_weather;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +19,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if(Build.VERSION.SDK_INT>=21){  //版本号判断（以下功能只在21及以上版本实现）
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_STABLE);   //活动的布局会显示在状态栏上面
+            getWindow().setStatusBarColor(Color.TRANSPARENT);   //将状态栏设置为透明色
+        }
         setContentView(R.layout.activity_main);
 
         if(NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE){
